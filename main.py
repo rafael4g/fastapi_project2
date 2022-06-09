d3a503655826f377108afefc4176c53be8f6cfc6
Rafael D Silva <neromad@outlook.com>
2022-06-09 15:53:30 -0300

    adding db models

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-from typing import List, Optional
-
-from pydantic import BaseModel
-
-from fastapi import FastAPI, Path, Query
+from fastapi import FastAPI
 
 from api import users, courses, sections
+from db.db_setup import engine
+from db.models import user, course
+
+user.Base.metadata.create_all(bind=engine)
+course.Base.metadata.create_all(bind=engine)
 
 app = FastAPI(
     title="Api-Fast",
