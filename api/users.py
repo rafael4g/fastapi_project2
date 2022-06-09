e24482a319a8f5222d682053495be5aadec73934
Rafael D Silva <neromad@outlook.com>
2022-06-09 17:59:34 -0300

    Adding injection dependence with pydantic schema

--- api/users.py
+++ api/users.py
@@ -1,32 +1,33 @@
-import fastapi
+from tkinter.messagebox import NO
 from typing import List, Optional
-from pydantic import BaseModel
+
+import fastapi
+from fastapi import Depends, HTTPException
+from sqlalchemy.orm import Session
+
+from db.db_setup import get_db
+from pydantic_schemas.user import UserCreate, User
+from api.utils.users import get_user, get_user_by_email, get_users, create_user
 
 
 router = fastapi.APIRouter()
 
 
-users = []
-
-
-class User(BaseModel):
-    email: str
-    is_active: bool
-    bio: Optional[str]
-
-
 @router.get("/users", response_model=List[User])
-async def get_users():
+async def read_users(skip: int = 0, limit: int =100, db: Session = Depends(get_db)):
+    users = get_users(db, skip=skip, limit=limit)
     return users
 
+@router.post("/users", response_model=User, status_code=201)
+async def create_new_user(user: UserCreate, db: Session = Depends(get_db)):  
+    db_user = get_user_by_email(db=db, email=user.email)
+    if db_user:
+        raise HTTPException(status_code=400, detail="Email is already registered!")
+    return create_user(db=db, user=user)
 
-@router.post("/users")
-async def create_user(user: User):
-    users.append(user)
-    return "Success"
-
-
-# Query(None, max_length=5) limit de caracteres
-@router.get("/users/{id}")
-async def get_user(id: int):
-    return {"user": users[id]}
+@router.get("/users/{user_id}", response_model=User)
+async def read_user(user_id: int, db: Session = Depends(get_db)):
+    db_user =  get_user(db=db, user_id=user_id)
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found!")
+    return db_user
